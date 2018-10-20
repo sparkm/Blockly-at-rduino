a0acfc30c1f17044edaa93a98f1bb81d04532aef
Spark M <sparkmythought@gmail.com>
2018-10-19 17:48:00 -0700

    Add support for Servo Motors used in SMT projects

--- blocks/smt/blocks_typing.js
+++ blocks/smt/blocks_typing.js
@@ -1,3 +1,27 @@
 Blockly.Blocks.smt_hcsr04.getBlockType = function() {
 	return Blockly.Types.NUMBER;
 };
+Blockly.Blocks.smt_servo_attach.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.smt_servo_move.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.smt_servo_read_degrees.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.smt_servo_attached.getBlockType = function() {
+	return Blockly.Types.BOOLEAN;
+};
+Blockly.Blocks.smt_servo_detach.getBlockType = function() {
+	return Blockly.Types.BOOLEAN;
+};
+Blockly.Blocks.smt_servo_detach.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.smt_servo_rot_continue.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.smt_servo_rot_continue.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
--- generators/arduino/smt.js
+++ generators/arduino/smt.js
@@ -24,3 +24,69 @@ Blockly.Arduino.smt_hcsr04 = function() {
   var code = 'distance_cm('+dropdown_triger_pin+','+dropdown_dist_pin+')';
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
+
+Blockly.Arduino.smt_servo_attach = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var value_degree = Blockly.Arduino.valueToCode(this, 'DEGREE', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+
+  Blockly.Arduino.includes_['define_servo'] = '#include <Servo.h>';
+  Blockly.Arduino.definitions_['var_servo' + dropdown_name] = 'Servo ' + dropdown_name + ';';
+  Blockly.Arduino.setups_['setup_servo_' + dropdown_name] = dropdown_name + '.attach(' + value_pin + ');';
+  return '';
+};
+
+Blockly.Arduino.smt_servo_move = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var value_degree = Blockly.Arduino.valueToCode(this, 'DEGREE', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+
+  var code = dropdown_name + '.write(' + value_degree + ');\n';
+  return code;
+};
+
+Blockly.Arduino.smt_servo_read_degrees = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+
+  var code = dropdown_name + '.read()';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.smt_servo_attached = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+
+  var code = dropdown_name+'.attached()';  
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.smt_servo_detach = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+  
+  var code = dropdown_name+'.detach();';
+  return code;
+};
+
+Blockly.Arduino.smt_servo_rot_continue = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var value_degree = Blockly.Arduino.valueToCode(this, 'SPEED', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+
+  var code = dropdown_name + '.write(' + value_degree + ');\n';
+  return code;
+};
+
+Blockly.Arduino.smt_servo_rot_continue_param = function() {
+  var value_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var degree = Blockly.Arduino.valueToCode(this, 'SPEED', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_etat = this.getFieldValue('ETAT');
+  var dropdown_name = this.getFieldValue('SERVO_NAME');
+  
+  if (dropdown_etat == "FORWARD") var value_degree = 90 + parseInt(degree);
+  if (dropdown_etat == "BACKWARD") var value_degree = 90 - parseInt(degree);
+
+  var code = dropdown_name + '.write(' + value_degree + ');\n';
+  return code;
+};
